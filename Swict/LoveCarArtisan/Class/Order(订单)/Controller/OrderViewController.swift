66be539f0dd6fc6a90ade18806c2e8c59e26b92e
弓杰 <734907441@qq.com>
2017-07-18 10:14:59 +0800

    修改。

--- Swict/LoveCarArtisan/Class/Order(订单)/Controller/OrderViewController.swift
+++ Swict/LoveCarArtisan/Class/Order(订单)/Controller/OrderViewController.swift
@@ -1,82 +1,69 @@
 //
-//  NewsViewController.swift
-//  Text
+//  OrderViewController.swift
+//  LoveCarArtisan
 //
-//  Created by apple on 2017/4/20.
+//  Created by apple on 2017/7/5.
 //  Copyright © 2017年 apple. All rights reserved.
 //
 
 import UIKit
 
-class NewsViewController: RootViewController {
+class OrderViewController: RootViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // 布局NAV
-        newsLayoutNAV()
-        // 添加视图
-        addNewsLayoutView()
-    }
-    // MARK: 网络请求
-    
-    // MARK: 按钮点击方法
-    func newsButtonAction(button: UIButton) {
-        
-    }
-    // MARK: 布局NAV
-    private func newsLayoutNAV() {
-        
-    }
     // MARK: 创建控件
-    /** 消息table */
-    lazy var newsTable: UITableView = {
+    /** 订单table */
+    lazy var orderTable: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView.init(frame: CGRect.zero)
         tableView.backgroundColor = UIColor.clear
         tableView.separatorStyle = UITableViewCellSeparatorStyle.none
-//        tableView.estimatedRowHeight = 300
-//        tableView.rowHeight = UITableViewAutomaticDimension
         tableView.rowHeight = 150
         return tableView
     }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 布局NAV
+        orderLayoutNAV()
+        // 添加视图
+        addOrderLayoutView()
+    }
+    // MARK: 布局NAV
+    private func orderLayoutNAV() {
+        
+    }
     // MARK: 添加视图
-    private func addNewsLayoutView() {
-        /** 消息table */
-        view.addSubview(newsTable)
+    private func addOrderLayoutView() {
+        /** 订单table */
+        view.addSubview(orderTable)
     }
     // MARK: 布局视图
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        /** 消息table */
-        newsTable.mas_makeConstraints { (make:MASConstraintMaker!) in
+        /** 订单table */
+        orderTable.mas_makeConstraints { (make:MASConstraintMaker!) in
             make.top.mas_equalTo()(self.view.mas_top)
             make.left.mas_equalTo()(self.view.mas_left)
             make.right.mas_equalTo()(self.view.mas_right)
             make.bottom.mas_equalTo()(self.view.mas_bottom)
         }
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
 
-
-extension NewsViewController : UITableViewDelegate, UITableViewDataSource {
+extension OrderViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellID = "newsCell"
-        let cell: NewsCell = NewsCell(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
+        let cellID = "orderCell"
+        let cell: OrderCell = OrderCell(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
         return cell
     }
 }
-
-
-
-
-
 
---
+++ Swict/LoveCarArtisan/Class/Order(订单)/View/OrderCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderCell.swift
+//  LoveCarArtisan
+//
+//  Created by apple on 2017/7/5.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import UIKit
+
+class OrderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
